0906a0a1877649b2f41a034f30977702e8906fca
lucille <lucille@charliecullus>
2017-04-13 14:11:15 +0200

    Encodage fichiers txt

--- Projet_TAL.py
+++ Projet_TAL.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
+from __future__ import unicode_literals
 
 import nltk
 import os
@@ -8,36 +9,24 @@ import numpy
 import nltk.corpus  
 from nltk.text import Text
 import re
-#from nltk import FreqDist
+import sys
 
+#NB : utiliser seulement corpus.txt, c'est le seul fichier encodé correctement
 
-# sélection du corpus (1ere méthode) [instancier sous la forme d'un objet texte] 
-#coller le corpus dans /home/lucille/nltk_data/corpora/gutenberg
-text = Text(nltk.corpus.gutenberg.words('corpus.txt'))
-#Affichage de l'objet : print(text)
-
-#sélection du corpus (2e méthode)
-os.chdir("/home/lucille/Bureau/sciences co/Projets/TAL/Projet_TAL")
-f = open("corpus2.txt", "r")
-raw = f.read()
-#pour afficher le corpus : 
-#print(raw)
-#tentative de le convertir sous la forme d'un objet texte :
-#text2=Text(raw)
-# Affichage de l'objet : print(text2)
-#Cela fonctionne mais les caractères sont espacés et donc il ne matche rien quand on
-#utilise des fonctions prenant en paramètres des objets textes (vous pouvez chercher comment
-#résoudre le problème si vous le souhaitez)
-
-#3e alternative :
-with open('corpus2.txt', 'r') as f:
+#Importation du fichier :
+#os.chdir("/home/lucille/Bureau/TAL")
+with open('corpus.txt', 'r') as f:
     sample = f.read()
-sample = unicode(sample, errors='ignore')
 
+#Vérifications :
+num=str(len(sample))
+print("Le fichier contient "+num+" caractères.")
 
-#ATTENTION : NE TRAVAILLER QU'AVEC "TEXT" (problème d'encodage des mots avec raw)
-tokens = nltk.word_tokenize(sample)
-print(tokens)
+#Tokenization en mots : 
+expr = re.compile("\W+",re.U)
+liste = expr.split(sample)
+print("Le fichier contient "+str(len(liste))+" mots.\n")
 
-
-
+#Exécution d'une fonction de concordance : 
+liste=Text(liste)
+liste.concordance("murder")
