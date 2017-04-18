e7e14cf26862c722cb22d76d882a92be9e5f2240
WorkPlace <workplace@MacBook-Pro-de-Nicolas.local>
2017-04-18 20:01:24 +0200

    Tokenizatino

--- Projet_TAL.py
+++ Projet_TAL.py
@@ -6,31 +6,40 @@ from __future__ import unicode_literals
 import nltk
 import os
 import numpy
-import nltk.corpus  
+import nltk.corpus
 from nltk.text import Text
 import re
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 #NB : utiliser seulement corpus.txt, c'est le seul fichier encodé correctement
 
 #Importation du fichier :
-os.chdir("/Users/workplace/Documents/Tal2017")
-with open('corpus.txt', 'r') as f:
-    sample = f.read()
+#os.chdir("/Users/workplace/Documents/Tal2017")
+#with open('corpus.txt', 'r') as f:
+#   sample = f.read()
 
-#Vérifications :
-num=str(len(sample))
-print("Le fichier contient "+num+" caractères.")
+folder = nltk.data.find("/Users/workplace/Documents/Tal2017")
+corpusReader = nltk.corpus.PlaintextCorpusReader(folder, 'corpus.txt')
 
-#Tokenization en mots : 
-expr = re.compile("\W+",re.U)
-liste = expr.split(sample)
-print("Le fichier contient "+str(len(liste))+" mots.\n")
+#Tokenization en phrases :
+nb_sentences = len(corpusReader.sents())
+print("le fichier contient "+ str(nb_sentences) +" phrases")
+
+#Tokenization en paragraphs :
+nb_paragraphs = len(corpusReader.paras())
+print("le fichier contient "+ str(nb_sentences) +" paragraphs")
+
+#Tokenization en mots :
+nb_words=len([word for sentence in corpusReader.sents() for word in sentence])
+print("Le fichier contient "+ str(nb_words) +" mots.")
+
+#Tokenization en caractaires :
+nb_chars=len([char for sentence in corpusReader.sents() for word in sentence for char in word])
+print("Le fichier contient "+ str(nb_chars) +" caractères.\n")
+
+
 
 #Exécution d'une fonction de concordance : 
-liste=Text(liste)
-liste.concordance("murder")
+#liste=Text(liste)
+#liste.concordance("murder")
 
